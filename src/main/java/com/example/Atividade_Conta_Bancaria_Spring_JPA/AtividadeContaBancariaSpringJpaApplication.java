46eb3daa38f7c7ccb551c8c5d811daf3d3603e1c
EricaSOS <erica.sos.adv@hotmail.com>
2025-06-15 20:58:15 -0300

    feat: adiciona a estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/AtividadeContaBancariaSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atividade_Conta_Bancaria_Spring_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeContaBancariaSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeContaBancariaSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/AtividadeContaBancariaSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Atividade_Conta_Bancaria_Spring_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeContaBancariaSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

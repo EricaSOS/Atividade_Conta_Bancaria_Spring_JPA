68172cf5cee8be3dae26d3a6b14bdbf5e03d280a
EricaSOS <erica.sos.adv@hotmail.com>
2025-06-17 21:29:42 -0300

    feat: Inclusão da classe Controlller ou cControlador

---
+++ src/main/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/controlador/ControladorContaBancaria.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.Atividade_Conta_Bancaria_Spring_JPA.controlador;
+
+import com.example.Atividade_Conta_Bancaria_Spring_JPA.modelo.ContaBancaria;
+import com.example.Atividade_Conta_Bancaria_Spring_JPA.repositorio.RepositorioContaBancaria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contas")
+
+public class ControladorContaBancaria {
+    @Autowired
+    private RepositorioContaBancaria repositorio;
+
+    @GetMapping
+    public List<ContaBancaria> listar() {
+        return repositorio.findAll();
+    }
+
+    @PostMapping
+    public ContaBancaria criar(@RequestBody ContaBancaria conta) {
+        return repositorio.save(conta);
+    }
+
+    @PutMapping("/{id}/deposito")
+    public ContaBancaria depositar(@PathVariable Long id, @RequestParam float valor) {
+        ContaBancaria conta = repositorio.findById(id).orElseThrow();
+        conta.deposita(valor);
+        return repositorio.save(conta);
+    }
+
+    @PutMapping("/{id}/retirada")
+    public ContaBancaria retirar(@PathVariable Long id, @RequestParam float valor) {
+        ContaBancaria conta = repositorio.findById(id).orElseThrow();
+        conta.retirada(valor);
+        return repositorio.save(conta);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        repositorio.deleteById(id);
+    }
+}
+

ece0ea81e3a3ff9548efa5ea327769ecf2326c8e
EricaSOS <erica.sos.adv@hotmail.com>
2025-06-17 21:17:19 -0300

    feat: Inclusão dos pacotes model e repositorio da conta bacaria

---
+++ src/main/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/model/ContaBancaria.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.Atividade_Conta_Bancaria_Spring_JPA.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ContaBancaria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeCliente;
+    private float saldo;
+
+    public ContaBancaria() {
+    }
+
+    public ContaBancaria(String nomeCliente, float saldo) {
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public void deposita(float valor) {
+        this.saldo += valor;
+    }
+
+    public void retirada(float valor) {
+        this.saldo -= valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/main/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/repositorio/RepositorioContaBancaria.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.example.Atividade_Conta_Bancaria_Spring_JPA.repositorio;
+
+import com.example.Atividade_Conta_Bancaria_Spring_JPA.model.ContaBancaria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioContaBancaria extends JpaRepository<ContaBancaria, Long>{
+    
+}

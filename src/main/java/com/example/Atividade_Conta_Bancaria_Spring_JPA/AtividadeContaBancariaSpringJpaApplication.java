d525851402b119b89f1f445783cc938a79832c3d
EricaSOS <erica.sos.adv@hotmail.com>
2025-06-20 20:19:46 -0300

    feat: Inserção de mensagem de servidor iniciado e como parar o servidor

--- src/main/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/AtividadeContaBancariaSpringJpaApplication.java
+++ src/main/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/AtividadeContaBancariaSpringJpaApplication.java
@@ -1,13 +1,21 @@
 package com.example.Atividade_Conta_Bancaria_Spring_JPA;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AtividadeContaBancariaSpringJpaApplication {
+public class AtividadeContaBancariaSpringJpaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtividadeContaBancariaSpringJpaApplication.class, args);
 	}
+        
+        @Override
+        public void run(String... args) {
+            System.out.println("🌐 Servidor iniciado com sucesso!");
+            System.out.println("🔗 Acesse: http://localhost:8080/contas");
+            System.out.println("💡 Para parar o servidor, pressione CTRL + C.");
+        }
 
 }

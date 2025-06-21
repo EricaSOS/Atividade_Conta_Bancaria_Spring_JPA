3d2b23d0f4226a37ac129c593ee4212bd649302f
EricaSOS <erica.sos.adv@hotmail.com>
2025-06-20 21:16:31 -0300

    feat: Inclusão do Controlador da Conta Bancaria Versionada

---
+++ src/main/java/com/example/Atividade_Conta_Bancaria_Spring_JPA/versionado/controlador/ControladorContaBancariaVersionada.java
@@ -0,0 +1,51 @@
+package com.example.Atividade_Conta_Bancaria_Spring_JPA.versionado.controlador;
+
+import com.example.Atividade_Conta_Bancaria_Spring_JPA.versionado.modelo.ContaBancariaVersionada;
+import com.example.Atividade_Conta_Bancaria_Spring_JPA.versionado.repositorio.RepositorioContaBancariaVersionada;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/contas-versionadas")
+
+public class ControladorContaBancariaVersionada {
+    @Autowired
+    private RepositorioContaBancariaVersionada repositorio;
+
+    @GetMapping
+    public List<ContaBancariaVersionada> listar() {
+        return repositorio.findAll();
+    }
+
+    @PostMapping
+    public ContaBancariaVersionada criar(@RequestBody ContaBancariaVersionada conta) {
+        return repositorio.save(conta);
+    }
+
+    @PutMapping("/{id}/deposito")
+    public ContaBancariaVersionada depositar(@PathVariable Long id, @RequestParam float valor) {
+        ContaBancariaVersionada conta = repositorio.findById(id).orElseThrow();
+        conta.deposita(valor);
+        return repositorio.save(conta);
+    }
+
+    @PutMapping("/{id}/retirada")
+    public ContaBancariaVersionada retirar(@PathVariable Long id, @RequestParam float valor) {
+        ContaBancariaVersionada conta = repositorio.findById(id).orElseThrow();
+        conta.retirada(valor);
+        return repositorio.save(conta);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        repositorio.deleteById(id);
+    }
+
+    // Coloquei um tratamento de concorrência
+    @ExceptionHandler(OptimisticLockingFailureException.class)
+    public String conflitoConcorrencia() {
+        return ">>>️ Conflito de concorrência detectado! A conta foi modificada por outro processo.";
+    }
+}
